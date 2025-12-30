045f160a46de207777320fb3868b06999cc17c62
pujitha-ccc <cr102@cccsnv.com>
2025-12-30 11:04:17 +0530

    feat: migrated normal css to tailwind css

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,17 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body
-        className="
-          min-h-screen
-          bg-[#f5f5f5]
-          flex
-          items-center
-          justify-center
-          p-5
-          font-sans
-        "
-      >
+      <body className="min-h-screen bg-[#f5f5f5] flex items-center justify-center p-5font-sans">
         {children}
       </body>
     </html>
